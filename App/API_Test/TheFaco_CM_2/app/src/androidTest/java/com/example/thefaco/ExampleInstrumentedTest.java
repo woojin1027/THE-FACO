9778d89194911b98efe2eff5d54614896a630db2
leyujin1007 <leyujin1007@naver.com>
2020-07-07 22:14:24 +0900

    stationId 조회용

--- App/API_Test/TheFaco_CM_2/app/src/androidTest/java/com/example/thefaco/ExampleInstrumentedTest.java
+++ App/API_Test/TheFaco_CM_2/app/src/androidTest/java/com/example/thefaco/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.theFaCo_App;
+package com.example.thefaco;
 
 import android.content.Context;
 
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.theFaCo_App", appContext.getPackageName());
+
+        assertEquals("com.example.thefaco", appContext.getPackageName());
     }
 }
