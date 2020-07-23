f674a1835a26e69913ded30d9de25acd284f695b
leyujin1007 <leyujin1007@naver.com>
2020-07-23 23:19:52 +0900

    App_0723_1

--- Mypractice/app/src/main/java/com/example/practice/Frag3.java
+++ Mypractice/app/src/main/java/com/example/practice/Frag3.java
@@ -1,9 +1,11 @@
 package com.example.practice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +22,25 @@ public class Frag3 extends Fragment // Fragment 클래스를 상속받아야한�
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.frag3,container,false);
+
+        Button button_setting1 = view.findViewById(R.id.button_setting1);
+        Button button_setting2 = view.findViewById(R.id.button_setting2);
+
+        //버튼 클릭시 PathSetting으로 이동
+        button_setting1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),PathSetting.class);
+                startActivity(intent);
+            }});
+
+        button_setting2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),PathSetting.class);
+                startActivity(intent);
+            }});
+
         return view;
     }
 }
