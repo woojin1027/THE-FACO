cce29766d370d045cac3c0cd537761cb09718417
leyujin1007 <leyujin1007@naver.com>
2020-08-12 19:47:38 +0900

    app_0812

--- Mypractice/app/src/main/java/com/example/practice/BackPressCloseHandler.java
+++ Mypractice/app/src/main/java/com/example/practice/BackPressCloseHandler.java
@@ -1,6 +1,12 @@
 package com.example.practice;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BackPressCloseHandler extends Activity{
@@ -25,7 +31,20 @@ public class BackPressCloseHandler extends Activity{
     }
 
     private void showGuide() {
-        toast = Toast.makeText(activity, "\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
-        toast.show();
+//        LayoutInflater inflater = getLayoutInflater();
+//        View layout = inflater.inflate( R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_layout));
+//        TextView text = layout.findViewById(R.id.text);
+//        Toast toast = new Toast(this);
+//        text.setText("\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.");
+//        text.setTextSize(15);
+//        text.setTextColor(Color.WHITE);
+//        toast.setGravity(Gravity.BOTTOM,0,0);
+//        toast.setDuration(Toast.LENGTH_SHORT);
+//        toast.setView(layout);
+//        toast.show();
+//
+
+          toast = Toast.makeText(activity, "\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+          toast.show();
     }
 }
--- Mypractice/app/src/main/java/com/example/practice/pathsetting_practice.java
+++ Mypractice/app/src/main/java/com/example/practice/pathsetting_practice.java
@@ -1,18 +1,15 @@
 package com.example.practice;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,9 +17,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -30,14 +24,10 @@ import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
-//해야할것 : 정류장 클릭 시 버튼(이나 텍스트뷰)에 띄우게 바꾸기
-
-
-public class pathSetting_start extends AppCompatActivity implements TextWatcher {
+public class pathsetting_practice extends AppCompatActivity implements TextWatcher {
+    private ListView list_excel;
     EditText searchBox;
-    ListView list_excel;
-    ArrayAdapter<String> arrayAdapter;
-    TextView textView;
+    final List<String[]> listItems = new LinkedList<String[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,28 +36,11 @@ public class pathSetting_start extends AppCompatActivity implements TextWatcher 
 
         searchBox = (EditText)findViewById(R.id.searchbox);  //검색창
         list_excel = (ListView)findViewById(R.id.list_excel);  //정류장데이터
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        //텍스트뷰 한개로 구성된 내장 레이아웃
 
-        Excel(); //데이터 읽기
-
-        list_excel.setAdapter(arrayAdapter);
+        Excel();
+        list_excel.setAdapter((ListAdapter) listItems);
         list_excel.setTextFilterEnabled(true);
         searchBox.addTextChangedListener(this);
-
-
-        //정류장 클릭시 이벤트
-        list_excel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selected_item = (String)parent.getItemAtPosition(position);
-                textView = findViewById(R.id.textview_setting1);
-                //textView.setText(selected_item); 출발지 텍스트가 안바뀐다.......ㅠ
-                toastshow(view, selected_item);
-                pathSetting_start.super.onBackPressed();
-            }
-        });
     }
 
     private void Excel() {
@@ -80,34 +53,83 @@ public class pathSetting_start extends AppCompatActivity implements TextWatcher 
 
             int MaxColumn = 2;
             int RowStart = 1; //B열
-            int RowEnd = sheet.getColumn(MaxColumn - 1).length -1;
+            int RowEnd = sheet.getColumn(MaxColumn - 1).length - 1;
             int ColumnStart = 1; //2행부터
             int ColumnEnd = sheet.getRow(2).length - 1;
 
 
             //excel file의 B2셀부터 B열의 모든 정보를 띄움 ( = 2019년 기준 경기도에 있는 모든 버스정류장 이름)
-            for(int row = RowStart; row <= RowEnd; row++) {
+            for (int row = RowStart; row <= RowEnd; row++) {
                 String excelload = sheet.getCell(ColumnStart, row).getContents();
-                arrayAdapter.add(excelload);
+                int row2 = row + 6;
+                String excelload_2 = sheet.getCell(ColumnStart, row2).getContents();
+                listItems.add(new String[]{ excelload , excelload_2});
             }
-        } catch (IOException e) {
+        }catch (IOException e) {
             e.printStackTrace();
         } catch (BiffException e) {
             e.printStackTrace();
         } finally {
-            list_excel.setAdapter(arrayAdapter);
+            list_excel.setAdapter((ListAdapter) listItems);
             workbook.close();
         }
+
+
+//        final int size = 20;
+//        for(int i=0; i<size; i++){
+//            listItems.add(new String[] { "Main title "+i, "Sub title "+i });
+//        }
     }
 
+    private void initList() {
+        list_excel = (ListView) findViewById(R.id.list_excel);
+
+        ArrayAdapter<String []> adapter = new ArrayAdapter<String []>(this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                listItems){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                View row = super.getView(position, convertView, parent);
+
+                TextView text1 = (TextView) row.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) row.findViewById(android.R.id.text2);
+
+                String[] item = listItems.get(position);
+                text1.setText(item[0]);
+                text2.setText(item[1]);
+                return row;
+            }
+        };
+
+
+
+        list_excel.setAdapter(adapter);
+
+
+
+        list_excel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                String[] item = listItems.get(position);
+                String message = item[0]+"\n"+item[1];
+
+                Toast.makeText(getApplicationContext(),
+                        message,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         list_excel.setFilterText(searchBox.getText().toString());
+
     }
 
     @Override
@@ -116,24 +138,4 @@ public class pathSetting_start extends AppCompatActivity implements TextWatcher 
             list_excel.clearTextFilter();
         }
     }
-
-
-    //주변 정류장 검색
-    public void mynear(View view) {
-
-    }
-
-    public void toastshow(View view, String string) {
-        LayoutInflater inflater = getLayoutInflater();
-        View layout = inflater.inflate( R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_layout));
-        TextView text = layout.findViewById(R.id.text);
-        Toast toast = new Toast(this);
-        text.setText(string + "을(를) 선택");
-        text.setTextSize(15);
-        text.setTextColor(Color.WHITE);
-        toast.setGravity(Gravity.BOTTOM,0,0);
-        toast.setDuration(Toast.LENGTH_SHORT);
-        toast.setView(layout);
-        toast.show(); }
-
 }
