ad4072b1a88079747d620ccef5ea183dec82065f
leyujin1007 <leyujin1007@naver.com>
2020-08-19 19:41:17 +0900

    app_0819

--- Mypractice/app/src/main/java/com/example/practice/pathset_mapshow.java
+++ Mypractice/app/src/main/java/com/example/practice/pathset_mapshow.java
@@ -1,17 +1,16 @@
 package com.example.practice;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -20,29 +19,36 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.net.URL;
 
-public class pathset_mapshow extends Activity implements OnMapReadyCallback {
-    GoogleMap mapView;
+/*https://coding-factory.tistory.com/39*/
 
+public class pathset_mapshow extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap googleMap;
     private pathSetting_start pathSetting_start;
-    String string_x, string_y;
-    int int_x, int_y;
+    float float_x, float_y;
+    float Array[][];
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pathsetting);
         StrictMode.enableDefaults();
+        setContentView(R.layout.map_search);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_search);
+        mapFragment.getMapAsync(this);
 
-        TextView status1 = (TextView) findViewById(R.id.result); //파싱된 결과확인!
+
+        //TextView status1 = (TextView) findViewById(R.id.result2); //파싱된 결과확인
 
         boolean bool_x = false, bool_y = false;
 
+        String string_x, string_y;
+
 
         try {
-            URL url = new URL("http://openapi.gbis.go.kr/ws/rest/busstationservice?serviceKey="
+            URL url = new URL("http://openapi.gbis.go.kr/ws/rest/busstationservice?serviceKey=" //정류소명/번호목록조회
                     + "d6tEeUjm3AQ5KdyZhb2TVkcsfbM88hHVzwSaYUb4qRYG7N2Pzc9yw71hTeHUNmz7IUrf7GyX%2Ffe5hmgmn7qVqA%3D%3D"
                     + "&keyword="
-                    + pathSetting_start.selected_item
+                    + "미금"
             );
 
 
@@ -57,23 +63,49 @@ public class pathset_mapshow extends Activity implements OnMapReadyCallback {
             while (parserEvent != XmlPullParser.END_DOCUMENT) {
                 switch (parserEvent) {
                     case XmlPullParser.START_TAG://parser가 시작 태그를 만나면 실행
-                        if (parser.getName().equals("x")) {
-                            bool_x = true;
-                        }
-                        if (parser.getName().equals("y")) {
-                            bool_y = true;
+                        if (parser.getName().equals("busStationList")) {
+                            if (parser.getName().equals("x")) {
+                                bool_x = true;
+                            }
+                            if (parser.getName().equals("y")) {
+                                bool_y = true;
+                            }
+                            if(parser.getName().equals("message")){ //message 태그를 만나면 에러 출력
+                                Toast.makeText(this,          // 현재 화면의 제어권자
+                                        "error", // 보여줄 메시지
+                                        Toast.LENGTH_LONG)    // 보여줄 기간 (길게, 짧게)
+                                        .show();
+
+//                                status1.setText(status1.getText()+"에러");
+                                //여기에 에러코드에 따라 다른 메세지를 출력하도록 할 수 있다.
+                            }
                         }
                         break;
+
                     case XmlPullParser.TEXT://parser가 내용에 접근했을때
                         if (bool_x) {
                             string_x = parser.getText();
+                            float_x = Float.parseFloat(string_x);
                             bool_x = false;
-                            int_x = Integer.parseInt(string_x);
                         }
                         if (bool_y) {
                             string_y = parser.getText();
+                            float_y = Float.parseFloat(string_y);
                             bool_y = false;
-                            int_y = Integer.parseInt(string_y);
+                        }
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if(parser.getName().equals("msgbody")) {
+                            Toast.makeText(this, "\n 위도:" + float_x + "\n 경도:" + float_y + "\n", Toast.LENGTH_LONG).show();
+//      수정중                      status1.setText(status1.getText()+"\n 위도 : " + float_x + "\n 경도 : " + float_y +"\n");
+//                            int i = 0, j = 0;
+//                            for (i = 0, i <= 10, i++){
+//                                for (j = 0, j <= 10, j++) {
+//                                    Array[i][j] = Array[(float) float_x][float_y];
+//                                }
+//                            }
+//                            Array[i][j]
                         }
                         break;
                 }
@@ -81,73 +113,30 @@ public class pathset_mapshow extends Activity implements OnMapReadyCallback {
             }
 
         } catch (Exception e) {
-            status1.setText("에러발생");
+            Toast.makeText(this,"에러가 났습니다",Toast.LENGTH_LONG).show();
+//            status1.setText("에러가 났습니다");
         }
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng SEOUL = new LatLng(int_x, int_y);
-
+        LatLng SEOUL = new LatLng(37.56, 126.97);
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
         markerOptions.title("서울");
         markerOptions.snippet("한국의 수도");
-        mapView.addMarker(markerOptions);
-
-        mapView.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mapView.animateCamera(CameraUpdateFactory.zoomTo(10));
-
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+//     수정중   if (float_x != 0){
+//            do {
+//                LatLng marker[] = new LatLng[float_x, float_y];
+//                MarkerOptions markerOptions = new MarkerOptions();
+//
+//            }while();
+//        }
 
 
     }
 }
-
-
-//
-//    public void main(String[] args) throws IOException {
-//        StringBuilder urlBuilder = new StringBuilder("http://openapi.gbis.go.kr/ws/rest/busstationservice"); /*URL*/
-//        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=d6tEeUjm3AQ5KdyZhb2TVkcsfbM88hHVzwSaYUb4qRYG7N2Pzc9yw71hTeHUNmz7IUrf7GyX%2Ffe5hmgmn7qVqA%3D%3D"); /*Service Key*/
-//        urlBuilder.append("&" + URLEncoder.encode("keyword","UTF-8") + "=" + URLEncoder.encode(pathSetting_start.selected_item, "UTF-8")); /*정류소명 또는 번호(2자리이상)*/
-//        URL url = new URL(urlBuilder.toString());
-//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//        conn.setRequestMethod("GET");
-//        conn.setRequestProperty("Content-type", "application/json");
-//        System.out.println("Response code: " + conn.getResponseCode());
-//        BufferedReader rd;
-//
-//        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-//            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//        }
-//        else {
-//            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-//        }
-//        StringBuilder sb = new StringBuilder();
-//        String line;
-//        while ((line = rd.readLine()) != null) {
-//            sb.append(line);
-//        }
-//        rd.close();
-//        conn.disconnect();
-//        System.out.println(sb.toString());
-//
-//
-//
-//
-
-//
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//
-//        LatLng SEOUL = new LatLng(37.56, 126.97);
-//
-//        MarkerOptions markerOptions = new MarkerOptions();
-//        markerOptions.position(SEOUL);
-//        markerOptions.title("서울");
-//        markerOptions.snippet("한국의 수도");
-//        mapView.addMarker(markerOptions);
-//
-//        mapView.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-//        mapView.animateCamera(CameraUpdateFactory.zoomTo(10));
-//    }
-//}
