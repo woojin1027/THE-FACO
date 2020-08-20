15bbad76926ce9994839d2e21ef1badc245bbbbe
woojin1027 <woojinny@kakao.com>
2020-08-20 20:16:26 +0900

    open api

---
+++ AWS_Cloud/cal_test.py
@@ -0,0 +1,10 @@
+import requests
+import xmltodict
+
+key = "k9SIHsYmigNv2OONMs4CHstwWrnYUTa2rlIxPlvRLxoTRXJL7KmrH2%2B126GkdZLo5T1Jmf8BNbb8sFxV7QGqRw%3D%3D"
+
+url = "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey={}&busRouteId=100100221&startOrd=1&endOrd=13".format(key)
+
+content = requests.get(url).content
+dict = xmltodict.parse(content)
+print(dict)
