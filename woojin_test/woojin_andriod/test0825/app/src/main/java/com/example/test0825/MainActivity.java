268198a8e22f5699a158eb2b5393789bf2bf008b
woojin1027 <woojinny@kakao.com>
2020-08-25 19:35:16 +0900

    woojin-android-test

---
+++ woojin_test/woojin_andriod/test0825/app/src/main/java/com/example/test0825/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test0825;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView)findViewById(R.id.list);
+        List<String> data = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
+
+        data.add("우진");
+        data.add("android");
+        data.add("korean");
+        adapter.notifyDataSetChanged();
+    }
+}
