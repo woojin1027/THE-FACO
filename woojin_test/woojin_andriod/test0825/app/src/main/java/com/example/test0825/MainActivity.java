48d02a2aae59fb642bfc3529d67b60a28f574dca
woojin1027 <woojinny@kakao.com>
2020-08-26 13:33:07 +0900

    googlemap_test

--- woojin_test/woojin_andriod/test0825/app/src/main/java/com/example/test0825/MainActivity.java
+++ woojin_test/woojin_andriod/test0825/app/src/main/java/com/example/test0825/MainActivity.java
@@ -2,26 +2,42 @@ package com.example.test0825;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private ListView list;
     EditText et_save;
     String shared = "file";
 
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
+
         list = (ListView)findViewById(R.id.list);
         List<String> data = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
@@ -38,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         String value = sharedPreferences.getString("hong", "");
         et_save.setText(value);
 
+
     }
     protected void onDestroy() {
         super.onDestroy();
@@ -47,6 +64,19 @@ public class MainActivity extends AppCompatActivity {
         String value = et_save.getText().toString();
         editor.putString("hong", value);
         editor.commit();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location= new LatLng(37.388528, 127.124396);//AK 프라자
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("이매촌한신.서현역.AK프라자");
+        markerOptions.snippet("버스정류장");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,20));
 
     }
 }
