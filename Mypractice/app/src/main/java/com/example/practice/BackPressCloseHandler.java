261bcd7459bcc82e5233d8d8c41f37e5a3b36c81
leyujin1007 <leyujin1007@naver.com>
2020-08-28 03:25:01 +0900

    app_0828

--- Mypractice/app/src/main/java/com/example/practice/BackPressCloseHandler.java
+++ Mypractice/app/src/main/java/com/example/practice/BackPressCloseHandler.java
@@ -31,20 +31,7 @@ public class BackPressCloseHandler extends Activity{
     }
 
     private void showGuide() {
-//        LayoutInflater inflater = getLayoutInflater();
-//        View layout = inflater.inflate( R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_layout));
-//        TextView text = layout.findViewById(R.id.text);
-//        Toast toast = new Toast(this);
-//        text.setText("\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.");
-//        text.setTextSize(15);
-//        text.setTextColor(Color.WHITE);
-//        toast.setGravity(Gravity.BOTTOM,0,0);
-//        toast.setDuration(Toast.LENGTH_SHORT);
-//        toast.setView(layout);
-//        toast.show();
-//
-
-          toast = Toast.makeText(activity, "\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
-          toast.show();
+        toast = Toast.makeText(activity, "\'뒤로\'버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
