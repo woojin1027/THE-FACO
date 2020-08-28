ce89e68f0ff03404d1df2ba8ddc57cc323ca9656
woojin1027 <woojinny@kakao.com>
2020-08-28 13:06:43 +0900

    CHANGE LOGO!!

--- woojin_test/woojin_andriod/test0825/app/src/main/java/com/example/test0825/MainActivity.java
+++ woojin_test/woojin_andriod/test0825/app/src/main/java/com/example/test0825/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -30,10 +31,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private MapFragment mapFragment;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent = new Intent(this, activity_loading.class);
+        startActivity(intent);
         fragmentManager = getFragmentManager();
         mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.google_map);
         mapFragment.getMapAsync(this);
