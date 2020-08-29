344de61112d150d42523a9eb74d9f0a9cc1f8c50
woojin1027 <woojinny@kakao.com>
2020-08-30 07:27:33 +0900

    PULL은 선택이 아닌 '필수'

--- Mypractice/app/src/main/java/com/example/practice/MainActivity.java
+++ Mypractice/app/src/main/java/com/example/practice/MainActivity.java
@@ -204,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.navigation_menu2: //TheFaCo란?
                 //데이터 담아서 팝업(액티비티) 호출
                 Intent intent = new Intent(this, popupActivity.class);
-                intent.putExtra("data", "'THE FAstest COurse' 의 줄임말 입니다. \n\n이 앱의 취지는 현재의 대중교통 서비스앱과 비교하여, 가장 빠른 길을 알려주고, 위 앱의 사용자들또한, 각자의 이상에 가장 빠른길로 나아가는데 도움이 되었으면 하는 바램으로 선정하였습니다.");
+                intent.putExtra("data", "'THE FAstest COurse' 의 줄임말 입니다. \n 이 앱의 취지는 현재의 대중교통 서비스앱과 비교하여, 사용자에게 가장 빠른 길을 알려주고픈 마음으로 시작하였습니다. \n 이러한 생각을 알려드리기위해 위와 같은 명칭으로 앱을 제작하였습니다. ");
                 //value에 설명할 내용을 적으면 됨
 
                 startActivityForResult(intent, 1);
