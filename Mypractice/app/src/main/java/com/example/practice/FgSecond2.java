c1366b199582fe036a133024441e830b7ca5e9ed
leyujin1007 <leyujin1007@naver.com>
2020-09-03 22:44:43 +0900

    app_0903

--- Mypractice/app/src/main/java/com/example/practice/FgSecond2.java
+++ Mypractice/app/src/main/java/com/example/practice/FgSecond2.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
 
 public class FgSecond2 extends Fragment {
     // Store instance variables
@@ -38,8 +41,8 @@ public class FgSecond2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second2, container, false);
-        EditText tvLabel = (EditText) view.findViewById(R.id.editText22);
-        tvLabel.setText(title);
+        TextView tvLabel = (TextView) view.findViewById(R.id.textView_second2);
+        tvLabel.setText("대중교통 경로를 검색하세요!");
         return view;
     }
 }
